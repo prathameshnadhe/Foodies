6f360b9270cb961eb0d1a6d82fa8fe777895cbd6
prathameshnadhe <prathameshnadhe06@gmail.com>
2023-10-25 19:49:25 +0530

    Talk is cheap. show me the code: Foodie App

---
+++ App.js
@@ -0,0 +1,611 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./imgs/Foodies.jpg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="Foodies logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  const { restoData } = props;
+
+  const { name, cuisine, rating, cft } = restoData?.info;
+  return (
+    <div className="resto-card">
+      <img
+        className="resto-cover-img"
+        src={restoData.info.image.url}
+        alt="food_logo"
+      />
+      <div className="resto-card-data">
+        <h2>{name}</h2>
+        <h4>{cuisine.join(", ")}</h4>
+        <h4>{rating.aggregate_rating}</h4>
+        <h4>{cft.text}</h4>
+        <h4>{restoData.order.deliveryTime}</h4>
+      </div>
+    </div>
+  );
+};
+
+const restoList = [
+  {
+    type: "restaurant",
+    info: {
+      id: "1",
+      name: "McDonald's",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/1/10571/07aae713b8d0d951eaa489aa3bd02005_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "4.0",
+        rating_text: "4.0",
+        rating_subtitle: "Very Good",
+        rating_color: "5BA829",
+        votes: "2,024",
+        subtext: "REVIEWS",
+        is_new: false,
+      },
+      cft: {
+        text: "₹400 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "Shaniwar Peth, Pune",
+        address:
+          "Shop 5, 6, 7A, Ground Floor and Shop R1-1, R1-2, R1-3, R1-4, R1-5, First Floor, Mulay Chambers Co-operative Housing Society Limited, CTS 419, Narayan Peth, Near Shaniwar Peth, Pune",
+        localityUrl: "pune/shaniwar-peth-restaurants",
+      },
+      cuisine: ["Burger", "Fast Food", "Coffee", "Beverages"],
+    },
+    order: {
+      deliveryTime: "24 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/mcdonalds-shaniwar-peth/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "2",
+      name: "Pizza Hut",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/3/10503/7d1f0c24bf8d0ddf5aad917a9cc1d216_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "4.1",
+        rating_text: "4.1",
+        rating_subtitle: "Very Good",
+        rating_color: "5BA829",
+        votes: "1,154",
+        subtext: "REVIEW",
+        is_new: false,
+      },
+      cft: {
+        text: "₹700 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "Shukrawar Peth, Pune",
+        address:
+          "1, Final Plot. 47A, 1077 Shukrawar Peth, HBC Building, Near hirabaug chowk, Shukrawar Peth, Pune",
+        localityUrl: "pune/shukrawar-peth-restaurants",
+      },
+      cuisine: ["Pizza", "Pasta", "Fast Food", "Beverages"],
+    },
+    order: {
+      deliveryTime: "33 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/pizza-hut-shukrawar-peth/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "3",
+      name: "Moreshwar Foodee Corner",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/4/18490414/1f8ca0fe68cd66c43a8d54d466d9667f_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "4.1",
+        rating_text: "4.1",
+        rating_subtitle: "Very Good",
+        rating_color: "5BA829",
+        votes: "1,311",
+        subtext: "REVIEW",
+        is_new: false,
+      },
+
+      cft: {
+        text: "₹250 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "Sadashiv Peth, Pune",
+        address: "577/A, Rambaugh Chowk, Narayan Peth, Sadashiv Peth, Pune",
+        localityUrl: "pune/sadashiv-peth-restaurants",
+      },
+      cuisine: [
+        "Chinese",
+        "Sandwich",
+        "Pizza",
+        "Burger",
+        "Fast Food",
+        "Shake",
+        "Beverages",
+      ],
+    },
+    order: {
+      deliveryTime: "29 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/moreshwar-foodee-corner-sadashiv-peth/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "4",
+      name: "Supreme Sandwich Corner",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/1/6506151/0306a74f3abdb4d68985a5a34d53fed3_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "3.6",
+        rating_text: "3.6",
+        rating_subtitle: "Good",
+        rating_color: "9ACD32",
+        votes: "1,330",
+        subtext: "REVIEW",
+        is_new: false,
+      },
+      cft: {
+        text: "₹300 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "Budhwar Peth, Pune",
+        address: "Survey 34, Shop 4, Budhwar Peth, Pune",
+        localityUrl: "pune/budhwar-peth-restaurants",
+      },
+      cuisine: ["Fast Food", "Sandwich", "Pizza", "Beverages"],
+    },
+    order: {
+      deliveryTime: "33 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/supreme-sandwich-corner-budhwar-peth/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "5",
+      name: "Cafe Durga",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/1/12511/dd107b2bc15c96817c47ccb2bddb53a2_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "3.4",
+        rating_text: "3.4",
+        rating_subtitle: "Average",
+        rating_color: "CDD614",
+        votes: "35",
+        subtext: "REVIEWS",
+        is_new: false,
+      },
+
+      cft: {
+        text: "₹300 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "JM Road, Pune",
+        address: "Shop 2, Castle Plaza, Near JM Temple, JM Road, Pune",
+        localityUrl: "pune/jm-road-restaurants",
+      },
+      cuisine: [
+        "North Indian",
+        "South Indian",
+        "Sandwich",
+        "Pizza",
+        "Fast Food",
+        "Street Food",
+        "Cafe",
+        "Coffee",
+      ],
+    },
+    order: {
+      deliveryTime: "25 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/cafe-durga-1-jm-road/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "6",
+      name: "Kayani Bakery",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/9/12009/6fe6e1aca9f67b97c956c83c746ec70a_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "4.6",
+        rating_text: "4.6",
+        rating_subtitle: "Excellent",
+        rating_color: "3F7E00",
+        votes: "28.2K",
+        subtext: "REVIEWS",
+        is_new: false,
+      },
+      cft: {
+        text: "₹150 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "East Street, Pune",
+        address: "6, DEH, Koyani Road, Camp, East Street, Pune",
+        localityUrl: "pune/east-street-restaurants",
+      },
+      cuisine: ["Bakery"],
+    },
+    order: {
+      deliveryTime: "31 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/kayani-bakery-east-street/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "7",
+      name: "Filli Bites",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/4/18441514/41571cafa25df3e5afdd8de8238f7f11_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "4.0",
+        rating_text: "4.0",
+        rating_subtitle: "Very Good",
+        rating_color: "5BA829",
+        votes: "773",
+        subtext: "REVIEWS",
+        is_new: false,
+      },
+
+      cft: {
+        text: "₹400 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "Camp Area, Pune",
+        address: "Irani Imambara, Near Geeta Society, Camp Area, Pune",
+        localityUrl: "pune/camp-area-restaurants",
+      },
+      cuisine: ["Burger", "Lebanese", "Fast Food"],
+    },
+    order: {
+      deliveryTime: "35 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/filli-bites-camp-area/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "8",
+      name: "KFC",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/4/10624/337986458ed68cac8690991905bc68d5_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "3.4",
+        rating_text: "3.4",
+        rating_subtitle: "Average",
+        rating_color: "CDD614",
+        votes: "1,007",
+        subtext: "REVIEW",
+        is_new: false,
+      },
+
+      cft: {
+        text: "₹400 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "Parvati, Pune",
+        address:
+          "Plot 545, Shop 2A, 2B, 3, 3A, 3B & 4, Ground Floor & 1st Floor, Parvati, Pune",
+        localityUrl: "pune/parvati-restaurants",
+      },
+      cuisine: ["Burger", "Fast Food", "Biryani", "Desserts", "Beverages"],
+    },
+    order: {
+      deliveryTime: "50 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/kfc-parvati/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "9",
+      name: "Rolls Mania",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/6/11326/cd669b682419d986ba9e4550de43505a_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "4.1",
+        rating_text: "4.1",
+        rating_subtitle: "Very Good",
+        rating_color: "5BA829",
+        votes: "4,208",
+        subtext: "REVIEWS",
+        is_new: false,
+      },
+
+      cft: {
+        text: "₹350 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "Sadashiv Peth, Pune",
+        address: "Shop 3, 844/1, Vinayak Prasad Building, Sadashiv Peth, Pune",
+        localityUrl: "pune/sadashiv-peth-restaurants",
+      },
+      cuisine: ["Rolls", "Fast Food", "Wraps", "Beverages"],
+    },
+    order: {
+      deliveryTime: "28 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/rolls-mania-sadashiv-peth/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "10",
+      name: "KNS",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/8/6506988/82f36c57fd1c39cb8db1482fdca0be4f_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "3.9",
+        rating_text: "3.9",
+        rating_subtitle: "Good",
+        rating_color: "9ACD32",
+        votes: "34",
+        subtext: "REVIEWS",
+        is_new: false,
+      },
+
+      cft: {
+        text: "₹300 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "Deccan Gymkhana, Pune",
+        address:
+          "769/60 D, Mandeep Bagga House, Prabhat Road, Deccan Gymkhana, Pune",
+        localityUrl: "pune/deccan-gymkhana-restaurants",
+      },
+      cuisine: ["Fast Food", "Burger", "Sandwich", "Pizza", "Beverages"],
+    },
+    order: {
+      deliveryTime: "32 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/kns-deccan-gymkhana/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "11",
+      name: "Crazy Cheesy",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/3/18315713/673966f8686736ef386e96498054c8b4_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "4.3",
+        rating_text: "4.3",
+        rating_subtitle: "Very Good",
+        rating_color: "5BA829",
+        votes: "5,446",
+        subtext: "REVIEWS",
+        is_new: false,
+      },
+
+      cft: {
+        text: "₹400 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "Sadashiv Peth, Pune",
+        address:
+          "861, Sadashiv Peth, Dnyanprabodhini, Khau Galli, Rajwade Path, Sadashiv Peth, Pune",
+        localityUrl: "pune/sadashiv-peth-restaurants",
+      },
+      cuisine: [
+        "Pizza",
+        "Sandwich",
+        "Pasta",
+        "Fast Food",
+        "Desserts",
+        "Coffee",
+        "Shake",
+        "Beverages",
+      ],
+    },
+    order: {
+      deliveryTime: "29 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/crazy-cheesy-sadashiv-peth/order",
+      },
+    },
+  },
+  {
+    type: "restaurant",
+    info: {
+      id: "12",
+      name: "Subway",
+      image: {
+        url: "https://b.zmtcdn.com/data/pictures/chains/6/10556/f31733dfcef4c1edb388b77619e07a59_o2_featured_v2.jpg",
+      },
+      rating: {
+        has_fake_reviews: 0,
+        aggregate_rating: "3.7",
+        rating_text: "3.7",
+        rating_subtitle: "Good",
+        rating_color: "9ACD32",
+        votes: "9,569",
+        subtext: "REVIEWS",
+        is_new: false,
+      },
+
+      cft: {
+        text: "₹400 for two",
+      },
+      cfo: {
+        text: "₹200 for one",
+      },
+      locality: {
+        name: "FC Road, Pune",
+        address:
+          "Shop 3 & 4, CL Apartment, Plot 1216/6/1, Shivaji Nagar, FC Road, Pune",
+        localityUrl: "pune/fc-road-restaurants",
+      },
+      cuisine: [
+        "Healthy Food",
+        "Fast Food",
+        "Sandwich",
+        "Salad",
+        "Wraps",
+        "Beverages",
+      ],
+    },
+    order: {
+      deliveryTime: "31 min",
+      isServiceable: true,
+      hasOnlineOrdering: true,
+      actionInfo: {
+        text: "Order Now",
+        clickUrl: "/pune/subway-fc-road/order",
+      },
+    },
+  },
+];
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="resto-container">
+        {restoList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} restoData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
